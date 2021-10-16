f43e5f96bd9364806f2ea136e7e1bd9fb007e404
zuwannn <suwanneethongtha@gmail.com>
2021-10-16 22:27:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,52 @@
+/* import All library */
 const express = require('express')
-const app = express()
+const jwt = require('jwt-simple')
+const passport = require('passport')
+//ใช้ในการ decode jwt ออกมา
+const ExtractJwt = require("passport-jwt").ExtractJwt 
+//ใช้ในการประกาศ Strategy
+const JwtStrategy = require("passport-jwt").Strategy 
 
-const middleware = (req, res, next) => {
-    // ตรวจสอบว่า authorization คือ Boy หรือไม่
-    if(req.headers.authorization === "Boy")
-        next() // อนุญาตให้ไปฟังก์ชันถัดไป
-    else
-        res.send("ไม่อนุญาค")
+const app = express()
+app.use(express.json()) // ให้รับ json จาก body ได้
+
+const SECRET = "mysecretkey" //ในการใช้งานจริง คีย์นี้ให้เก็บเป็นความลับ
+
+//สร้าง Strategy
+const jwtOptions = {
+    jwtFromRequest : ExtractJwt.fromHeader("authorization"),
+    secretOrKey: SECRET //SECRETเดียวกับตอนencodeในกรณีนี้คือ mysecretkey
 }
-app.get('/', middleware, (req, res)=>{ // เพิ่มmiddlewareขั้นกลาง
-    res.send('ยอดเงินคงเหลือ 50')
+
+const jwtAuth = new JwtStrategy(jwtOptions, (payload, done) => {
+    if(payload.sub === "suwannee") done(null, true)
+    else done(null, false)
+})
+
+//เสียบ Strategy เข้า Passport
+passport.use(jwtAuth)
+
+//ทำ Passport Middleware
+const requireJWTAuth = passport.authenticate("jwt",{session:false});
+
+//เสียบ middleware ยืนยันตัวตน JWT เข้าไป
+app.get("/", requireJWTAuth, (req, res) => {
+    res.send("ยอดเงินคงเหลือ 50");
+ });
+
+//ทำ Middleware สำหรับขอ JWT
+const loginMiddleware = (req, res, next) => {
+    if(req.body.username === "suwannee" && 
+       req.body.password === "123") next()
+    else res.send("wrong username or password")
+}
+
+app.post('/login', loginMiddleware, (req, res) => {
+    const payload = {
+        sub: req.body.username,
+        iat: new Date().getTime() //มาจากคำว่า issued at time (สร้างเมื่อ)
+    }
+    res.send(jwt.encode(payload, SECRET))
 })
 
 app.listen(3000)
