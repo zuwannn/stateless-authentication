3be71d9c80ace81c97ea1bb010dbf563d718954b
zuwannn <suwanneethongtha@gmail.com>
2021-10-16 19:58:04 +0700

    Middleware

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send('hello world!')
+const middleware = (req, res, next) => {
+    // ตรวจสอบว่า authorization คือ Boy หรือไม่
+    if(req.headers.authorization === "Boy")
+        next() // อนุญาตให้ไปฟังก์ชันถัดไป
+    else
+        res.send("ไม่อนุญาค")
+}
+app.get('/', middleware, (req, res)=>{ // เพิ่มmiddlewareขั้นกลาง
+    res.send('ยอดเงินคงเหลือ 50')
 })
 
 app.listen(3000)
